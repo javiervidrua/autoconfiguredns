93696b585201460eaa7b8ea3ce81e6ae2197b329
javiervidrua <67796321+javiervidrua@users.noreply.github.com>
2020-09-23 11:11:39 +0200

    Update the usage function

--- autoconfiguredns.sh
+++ autoconfiguredns.sh
@@ -359,9 +359,9 @@ updateResolvConf(){
 
 # Prints the usage
 usage(){
-    echo 'Usage: ./dnser.sh <IN_IP_ADDRESS> <FULL_DOMAIN_NAME>'
+    echo 'Usage: ./autoconfiguredns.sh <IN_IP_ADDRESS> <FULL_DOMAIN_NAME>'
     echo ''
-    echo 'EXAMPLE: If the IP address of my network card is 192.168.1.11 and my FQDN is test.com, I will run: ./dnser.sh 192.168.1.11 test.com'
+    echo 'EXAMPLE: If the IP address of my network card is 10.1.2.3 and my FQDN is test.com, I will run: ./autoconfiguredns.sh 10.1.2.3 test.com'
     return 0
 }
 
